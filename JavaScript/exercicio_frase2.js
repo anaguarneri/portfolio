5b0c3873f06d14616d5c6c6d5d98b088dc7ae5ce
PGGKun <slunngdev@gmail.com>
2022-05-30 18:55:51 -0300

    subindo exercicio aula

---
+++ JavaScript/exercicio_frase2.js
@@ -0,0 +1,8 @@
+const minas = "Minas Programam";
+const professora = "Lissandra";
+const meses = "3";
+const raio= "8";
+const pi= Math.PI;
+const circunferencia = 2 * (pi * raio);
+const comprimento = true;
+console.log(`O curso ${minas} tem a duração de ${meses} e a professora ${professora} ensinou como calcular o comprimento de uma circunferência de raio ${raio} e o resultado era sempre ${circunferencia} o que resultava em ${comprimento}`);
---
+++ JavaScript/frases.js
@@ -0,0 +1,3 @@
+const frase1 = "Olá mundo";
+const frase2 = "Hello word";
+console.log(frase1 == frase2);
