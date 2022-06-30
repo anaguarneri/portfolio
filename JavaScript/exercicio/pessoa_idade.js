91ab3286bbcefefdaf5934d088822356664696f9
Ana Guarneri <anaguarnierie@gmail.com>
2022-06-30 19:39:57 -0300

    123

--- JavaScript/exercicio/pessoa_idade.js
+++
@@ -1,9 +0,0 @@
-let pessoa = prompt("Digite aqui seu nome: ");
-let idade = parseInt(prompt("Digite aqui sua idade: "));
-function dados ( pessoa, idade){
-    if(idade >18) {
-        return(` ${pessoa} é de maior e tem ${idade} anos.`)
-    }
-    else{ return(`${pessoa} é menor de idade e tem ${idade} anos.`);}
-}
-console.log(dados( pessoa, idade));
--- JavaScript/frases.js
+++
@@ -1,3 +0,0 @@
-const frase1 = "Olá mundo";
-const frase2 = "Hello word";
-console.log(frase1 == frase2);
